05b698f7284c3c0bad9ebec4906eaf0b4c38c90d
Stephane Cristini <stephane.crfernandes@gmail.com>
2020-04-19 00:23:27 -0300

    Desmonstrando funcionalidades do React: REF

--- ReactJS/primeiro_react/src/App.js
+++ ReactJS/primeiro_react/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Titulo texto="Texto através do atributo"></Titulo>
+        <Titulo></Titulo>
         <p>
           Esse é meu novo texto.
         </p>
--- ReactJS/primeiro_react/src/Titulo.js
+++ ReactJS/primeiro_react/src/Titulo.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
 
-//prop é a maneira com que o React recebe dados de entrada para um componente.
+//ref - referência feita para componentes ou elementos html
 class Titulo extends Component{
     constructor(props){
         super(props);
+        this.elementoH1 = React.createRef();
     }
+
+    componentDidMount(){
+        this.elementoH1.current.innerHTML = "Outro Texto";
+    }
+
     render(){
         return(
-            <h1>
-                {this.props.texto}
+            <h1 ref={this.elementoH1}>
+                Texto de Exemplo
             </h1>
         )
     }
